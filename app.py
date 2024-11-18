c2bb93e90bd53229a6cd278feeab284087e00a16
nencyfaganiya <nencyfaganiya.2910@gmail.com>
2024-11-18 16:18:56 +0000

    Bug fixing

--- app.py
+++ app.py
@@ -182,8 +182,9 @@ with col3:
 
 # Get files in directory
 if directory_path and Path(directory_path).exists():
+    st.write(f"Directory path: {directory_path}")
     items = []  # Initialize as an empty list at the start
-    st.write(f"Resolved path: {directory_path}")
+    
     # Resolve path if it's a mapped drive (e.g., Z:\folder)
     resolved_path = resolve_path(directory_path)
     st.write(f"Resolved path: {resolved_path}")
